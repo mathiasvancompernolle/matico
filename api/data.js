2fec1b7961b794b3178d17d6abd72737d235d65d
mathiasvancompernolle <mathiasvancompernolle@gmail.com>
2026-06-20 16:18:24 +0200

    Fix comment formatting in data.js

--- api/data.js
+++ api/data.js
@@ -1,4 +1,4 @@
-// v15-nasdaq-fix
+// v15-nasdaq-fix  
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
