2f104b083ba3b0eecccfce886cc1fb0f62205b60
mathiasvancompernolle <mathiasvancompernolle@gmail.com>
2026-06-20 18:17:58 +0200

    Update data.js

--- api/data.js
+++ api/data.js
@@ -996,7 +996,7 @@ const quoteResults = await Promise.all(syms.map(async (sym, idx) => {
           'QDVA.DE','EMUE.DE','WQDE.DE','IWDE.DE','IWDH.MI',
           'TNOW.PA','CW8.PA','MWRD.PA','CNDX.PA',
           'MEUD.L','MEUD.PA','LYP6.DE','DEFS.PA','ESE.PA','PABUS.MI','PAPU.MI','PAEEM.PA','PAASI.PA','PUST.PA','PANXG.PA',
-          'LYSP5.SW','C6E.PA','AME6.DE','CSE6.SW','EPAB.PA','PAEP.L',
+          'LYSP5.SW','C6E.PA','AME6.DE','CSE6.SW','EPAB.DE','PAEP.L',
           'EBLU.DE','BPAC.PA','CEUD.MI','BINFG.MI',
           'JPCT.MI','JPCS.MI','JREU.MI',
           'IHI','MLPX.MI',
