20e4258614262897405a328ed61a4190f6cbb0c2
mathiasvancompernolle <mathiasvancompernolle@gmail.com>
2026-06-22 23:02:15 +0200

    Update data.js

--- api/data.js
+++ api/data.js
@@ -1,5 +1,5 @@
 // v16-etf-beurs
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
