5d4e9ad6825eacb1195652abf776740782579f96
mathiasvancompernolle <mathiasvancompernolle@gmail.com>
2026-07-16 22:50:05 +0200

    Update AuthPage.jsx

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -18,6 +18,9 @@ export default function AuthPage({ onIngelogd }) {
       provider: 'google',
       options: {
         redirectTo: window.location.origin,
+        queryParams: {
+          prompt: 'select_account',
+        },
       },
     });
     if (error) { setFout(error.message); setLaden(false); }
