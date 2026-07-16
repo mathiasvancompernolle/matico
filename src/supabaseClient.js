dfd8b9b5e5712a45882461dad52f23939586e449
mathiasvancompernolle <mathiasvancompernolle@gmail.com>
2026-07-16 22:26:06 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.REACT_APP_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.REACT_APP_SUPABASE_ANON_KEY;
+const SUPABASE_URL = 'https://drdzcmxgqmohulngpgvk.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_Qpwzpsuy0Ed70CSBFu7Kug_BZFsZzrW';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
